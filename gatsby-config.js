31a519eaed4e57d94850555156034cca7841a803
Hoàng Tuấn <53977417+dirtycoins@users.noreply.github.com>
2021-02-23 01:34:02 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'ChandrikaDeb',
-        short_name: 'ChandrikaDeb',
+        name: 'Dirtycoins',
+        short_name: 'Dirtycoins',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
