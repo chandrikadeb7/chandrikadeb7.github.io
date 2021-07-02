6085a1aa7fe0a4f220d246f2ad9c9e11131485a2
Chandrika Deb <chandrikadeb7@gmail.com>
2021-07-02 12:14:46 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d3216d3632c5a76ec611.js"
+    "url": "webpack-runtime-5e3a2053e989819140b0.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e945ac6dae7c2f8865c3972ed7346010"
+    "revision": "4364d72be04182a6f81db85e8f73d8a4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
