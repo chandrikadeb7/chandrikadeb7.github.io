e9606abbedc512e48c9f0a7c4b577e4244a58687
xr7777 <71621322+xr7777@users.noreply.github.com>
2021-12-25 17:54:22 +0530

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -42,7 +42,7 @@ const PostTemplate = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>{title} | Chandrika Deb</title>
+        <title>{title} | XR7</title>
         <link rel="canonical" href="https://chandrikadeb7.github.io/pensieve" />
       </Helmet>
 
